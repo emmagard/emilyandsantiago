22c7c2a756e115ed52a47777492b54308d1e6078
Emma Gardner <emmagardner@outlook.com>
2015-05-13 16:43:16 -0400

    dded smooth scrolling

--- main.js
+++ main.js
@@ -1,81 +1,112 @@
-var map1, map2, map3;
+$(document).ready(function(){
 
-function initialize(){
+    //google maps
+    var map1, map2, map3;
 
-    var boston = new google.maps.LatLng(42.3631, -71.0064);
-    var prov =  new google.maps.LatLng(41.7239, -71.4283);
+    function initialize(){
 
-    var mapOptions1 = {
-        scrollwheel: false,
-        center: new google.maps.LatLng(42.3631, -71.0064),
-        zoom: 8
-    };
+        var boston = new google.maps.LatLng(42.3631, -71.0064);
+        var prov =  new google.maps.LatLng(41.7239, -71.4283);
 
-    var mapOptions2 = {
-        scrollwheel: false,
-        center: new google.maps.LatLng(41.7239, -71.4283),
-        zoom: 8
-    };
+        var mapOptions1 = {
+            scrollwheel: false,
+            center: new google.maps.LatLng(42.3631, -71.0064),
+            zoom: 8
+        };
 
-    var mapOptions3 = {
-        scrollwheel: false,
-        center: new google.maps.LatLng(42.010530, -71.271057),
-        zoom: 9
-    };
+        var mapOptions2 = {
+            scrollwheel: false,
+            center: new google.maps.LatLng(41.7239, -71.4283),
+            zoom: 8
+        };
 
-    map1 = new google.maps.Map(document.getElementById("boston-canvas"),
-        mapOptions1);
+        var mapOptions3 = {
+            scrollwheel: false,
+            center: new google.maps.LatLng(42.010530, -71.271057),
+            zoom: 9
+        };
 
-    map2 = new google.maps.Map(document.getElementById("prov-canvas"),
-        mapOptions2);
+        map1 = new google.maps.Map(document.getElementById("boston-canvas"),
+            mapOptions1);
 
-    map3 = new google.maps.Map(document.getElementById("events-map"),
-        mapOptions3);
+        map2 = new google.maps.Map(document.getElementById("prov-canvas"),
+            mapOptions2);
 
-    var marker1 = new google.maps.Marker({
-        position: boston,
-        map: map1,
-        title: 'Logan Airport'
-    });
+        map3 = new google.maps.Map(document.getElementById("events-map"),
+            mapOptions3);
 
-    var marker2 = new google.maps.Marker({
-        position: prov,
-        map: map2,
-        title: 'TF Green Airport'
-    });
-
-setEventMarkers(map3, events);
-
-}//end initialize
-
-var events = [
-    ["Providence T.F. Green Airport", 41.7239, -71.4283],
-    ["Boston Logan Airport", 42.3631, -71.0064],
-    ["Ceremony- St. Mary's", 41.671175, -71.270703],
-    ["Reception- Blithewold", 41.654135, -71.265996],
-    ["Newport", 41.490102, -71.312829],
-    ["Hotels- Middletown & Portsmouth", 41.525894, -71.296583]
-];
-
-function setEventMarkers(map, locations){
-    for (var i = 0; i < locations.length; i++) {
-        var event = locations[i];
-        var myLatLng = new google.maps.LatLng(event[1], event[2]);
-        var infowindow = new google.maps.InfoWindow({
-            content: event[0]
+        var marker1 = new google.maps.Marker({
+            position: boston,
+            map: map1,
+            title: 'Logan Airport'
         });
-        var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            animation: google.maps.Animation.DROP,
-            title: event[0],
-            infowindow: infowindow
+
+        var marker2 = new google.maps.Marker({
+            position: prov,
+            map: map2,
+            title: 'TF Green Airport'
         });
-        google.maps.event.addListener(marker, 'click', function() {
-          this.infowindow.open(map,this);
-        });
+
+    setEventMarkers(map3, events);
+
+    }//end initialize
+
+    var events = [
+        ["Providence T.F. Green Airport", 41.7239, -71.4283],
+        ["Boston Logan Airport", 42.3631, -71.0064],
+        ["Ceremony- St. Mary's", 41.671175, -71.270703],
+        ["Reception- Blithewold", 41.654135, -71.265996],
+        ["Newport", 41.490102, -71.312829],
+        ["Hotels- Middletown & Portsmouth", 41.525894, -71.296583]
+    ];
+
+    function setEventMarkers(map, locations){
+        for (var i = 0; i < locations.length; i++) {
+            var event = locations[i];
+            var myLatLng = new google.maps.LatLng(event[1], event[2]);
+            var infowindow = new google.maps.InfoWindow({
+                content: event[0]
+            });
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: event[0],
+                infowindow: infowindow
+            });
+            google.maps.event.addListener(marker, 'click', function() {
+              this.infowindow.open(map,this);
+            });
+        }
     }
-}
 
+    google.maps.event.addDomListener(window, 'load', initialize);
 
-google.maps.event.addDomListener(window, 'load', initialize);
+    //Check to see if JS and jQuery are working
+    console.log("js and query is working");
+
+    //Smooth scrolling
+    $('nav ul li a').on('click', function(event){
+        event.preventDefault();
+
+        var fullUrl = this.href;
+        console.log(this.href);
+        var separatedUrl = fullUrl.split('#');
+        var targ = separatedUrl[1];
+        var targetOffsetTop = $('#'+ targ).offset().top;
+
+        $('html, body').animate({
+          scrollTop: targetOffsetTop
+        }, 1500, "swing");
+    });
+
+    //Wedding day countdown
+    var weddingDay = new Date('09/05/2015');
+    console.log(weddingDay);
+    var now = new Date();
+    console.log(now);
+    var distance = weddingDay - now;
+    console.log(distance);
+    var daysRemaining = Math.floor(distance/86400000);
+    console.log(daysRemaining);
+});//end ready
